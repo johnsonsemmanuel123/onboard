2b1bc757063cf38cd22f353eb3388e53e07e1c4c
Emmanuel Johnson-Excellent <johnsonsexcellent@gmail.com>
2025-03-20 16:51:42 +0000

    second commit

--- view/inc/menu/growers.php
+++ view/inc/menu/growers.php
@@ -1,30 +1,66 @@
 <div class="sidebar" id="sidebar">
-<div class="sidebar-inner slimscroll">
-<div id="sidebar-menu" class="sidebar-menu">
-<ul>
-<li class="active">
-<a href="index.php?page=dashboard"><img src="../assets/img/icons/dashboard.svg" alt="img"><span> Dashboard</span> </a>
-</li>
-<li class="submenu">
-<a href="#">
-<img src="../assets/img/icons/product.svg" alt="img"><span>Profile [Grower]</span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="index.php?page=growerprofile">Update Profile</a></li>
-</ul>
-</li>
-<!-- <li class="submenu">
-<a href="javascript:void(0);"><img src="../assets/img/icons/sales1.svg" alt="img"><span> Category </span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="index.php?page=sub4">sub</a></li>
-</ul>
-</li> -->
-<li class="submenu">
-<a href="javascript:void(0);"><img src="../assets/img/icons/time.svg" alt="img"><span> Report</span> <span class="menu-arrow"></span></a>
-<ul>
-<li><a href="#index.php?page=reports">Generate Report</a></li>
-</ul>
-</li>
-</ul>
-</div>
-</div>
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="active">
+                    <a href="index.php?page=dashboard">
+                        <img src="../assets/img/icons/dashboard.svg" alt="img"><span> Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="submenu">
+                    <a href="#">
+                        <img src="../assets/img/icons/product.svg" alt="img"><span> Profile [Grower]</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="index.php?page=growerprofile">Update Profile</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#">
+                        <img src="../assets/img/icons/farm.svg" alt="img"><span> Farm Management</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="index.php?page=farm_management">Manage Farm</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#">
+                        <img src="../assets/img/icons/market.svg" alt="img"><span> Market Prices</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="index.php?page=market_prices">Price Trends</a></li>
+                        <li><a href="index.php?page=recommendations">Seasonal Recommendations</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#">
+                        <img src="../assets/img/icons/finance.svg" alt="img"><span> Finance</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="index.php?page=insurance">Insurance</a></li>
+                        <li><a href="index.php?page=savings">Savings</a></li>
+                        <li><a href="index.php?page=banking">Bank Account</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="#">
+                        <img src="../assets/img/icons/time.svg" alt="img"><span> Reports</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="index.php?page=reports">Generate Report</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
